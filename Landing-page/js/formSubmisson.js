c39d6ecb67f92cd91e4fb44bfb18044d04af3268
yuslove1 <adexyuslove@gmail.com>
2025-02-06 14:57:26 -0800

    styles: little style adjustment made

---
+++ Landing-page/js/formSubmisson.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (event) => {
+
+  event.preventDefault(); // Prevent default form submission
+  const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json' // 
+      }
+    })
+    .then(response => {
+      if (response.ok) {  // Check for successful response (status 200-299)
+        window.location.href = 'thankyou.html';
+        form.reset(); // Clear the form
+      } else {
+        return response.json().then(err => {throw new Error(err.message)});
+      }
+    })
+    .catch(error => {
+      alert("An error occurred: " + error.message);
+      console.error('Error:', error);
+    });
+  });
--- Landing-page/js/main.js
+++ Landing-page/js/main.js
@@ -9,10 +9,10 @@ toggleButtons.forEach((button) => {
     const answer = button.closest(".question").querySelector(".answer");
     answer.classList.toggle("show");
     if (button.classList.contains("expanded")) {
-      button.src = "images/arrow-down.svg";
+      button.src = "/images/arrow-down.svg";
       button.classList.remove("expanded");
     } else {
-      button.src = "images/arrow-up.svg";
+      button.src = "/images/arrow-up.svg";
       button.classList.add("expanded");
     }
   });
@@ -22,43 +22,16 @@ menuToggle.addEventListener('click', () => {
   menu.classList.toggle('active');
   btn.classList.toggle('active');
   if(menuToggle.classList.contains("expanded")){
-    menuToggle.src = "images/toggle-button.svg"
+    menuToggle.src = "/images/toggle-button.svg"
     menuToggle.classList.remove("expanded")
   }else{
-    menuToggle.src = "images/toggle-close-x-svgrepo-com.svg"
+    menuToggle.src = "/images/toggle-close-x-svgrepo-com.svg"
     menuToggle.classList.add("expanded")
     }
   }
 );
 
 
-const form = document.getElementById('myForm');
-
-form.addEventListener('submit', (event) => {
-
-  event.preventDefault(); // Prevent default form submission
-  const formData = new FormData(form);
-
-    fetch(form.action, {
-      method: 'POST',
-      body: formData,
-      headers: {
-        'Accept': 'application/json' // 
-      }
-    })
-    .then(response => {
-      if (response.ok) {  // Check for successful response (status 200-299)
-        window.location.href = 'thankyou.html';
-        form.reset(); // Clear the form
-      } else {
-        return response.json().then(err => {throw new Error(err.message)});
-      }
-    })
-    .catch(error => {
-      alert("An error occurred: " + error.message);
-      console.error('Error:', error);
-    });
-  });
 
 
 // const currentPath = window.location.pathname; 

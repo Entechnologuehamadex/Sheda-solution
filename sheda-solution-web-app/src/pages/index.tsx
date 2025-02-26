5d022c5740a09a44cdaef952ee0392b6cb61b33e
yuslove1 <adexyuslove@gmail.com>
2025-02-26 12:56:34 -0800

    chore: scaffolded the app

---
+++ sheda-solution-web-app/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className=" text-red-500 bg-white py-8 font-inter text-lg"><h1>Hello Welcome to sheda</h1></div>
+      
+    </div>
+  );
+}

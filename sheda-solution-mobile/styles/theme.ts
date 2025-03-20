c0e5cc4ec7cac44996821d278697d88ad7842bc0
yuslove1 <adexyuslove@gmail.com>
2025-03-20 14:16:04 -0700

    chore: scaffolded and config sheda mobile-app

---
+++ sheda-solution-mobile/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+      primary: "#D32F2F",
+      background: "#FFFFFF",
+      text: "#000000",
+      secondaryText: "#666666",
+      cardBackground: "#F5F5F5",
+      active: "#FFE6E6",
+    },
+    spacing: { sm: 8, md: 16, lg: 24 },
+  };  

1adac27112ee7cca20fcbb0321644b0e8cda0e02
yuslove1 <adexyuslove@gmail.com>
2025-03-21 14:49:22 -0700

    feat: auth components placeholder

---
+++ sheda-solution-mobile/app/(auth)/Login/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+const Login = () => {
+    return(
+        <View>
+        <Text>Welcome to login page</Text>
+        <Text>Welcome to login page</Text>
+        <Text>Welcome to login page</Text>
+        <Text>Welcome to login page</Text>
+    </View>
+    )
+  
+}
+
+export default Login;
---
+++ sheda-solution-mobile/app/(auth)/Signup/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+const Signup = () => {
+  return (
+    <View className="">
+      <Text>Welcome to Signup page</Text>
+      <Text>Welcome to Signup page</Text>
+      <Text>Welcome to Signup page</Text>
+      <Text>Welcome to SignUp page</Text>
+    </View>
+  );
+};
+
+export default Signup;

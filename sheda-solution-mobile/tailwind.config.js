29955f91f881a98264571d9f2da1f8aeade76997
yuslove1 <adexyuslove@gmail.com>
2025-03-21 14:48:09 -0700

    chore: configured tailwind to include our default themes

--- sheda-solution-mobile/tailwind.config.js
+++ sheda-solution-mobile/tailwind.config.js
@@ -1,19 +1,28 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./app/**/*.{js,jsx.ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#C1272D",
+        background: "#FFFFFF",
+        primaryText: "#000000",
+        secondaryText: "#878787",
+        cardBackground: "#F5F5F5",
+        active: "#FFE6E6",
+      },
+      spacing: {
+        sm: 8,
+        md: 16,
+        lg: 24,
+      },
+    },
   },
   plugins: [],
-}
+};

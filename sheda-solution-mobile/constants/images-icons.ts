6cedb0a34bb6b8f8d2eb3a7425eedaa3d0be4855
yuslove1 <adexyuslove@gmail.com>
2025-03-23 11:57:44 -0700

    file: constant data

---
+++ sheda-solution-mobile/constants/images-icons.ts
@@ -0,0 +1,14 @@
+const HOMEICON = require('../assets/icons/home-icon.svg');
+const HISTORYICON = require('../assets/icons/history-icon.svg');
+const INBOXICON = require('../assets/icons/inbox-icon.svg');
+const WALLETICON = require('../assets/icons/wallet-icon.svg');
+
+const HERO = require('../assets/images/hero.png')
+
+export {
+    HOMEICON,
+    HISTORYICON,
+    INBOXICON,
+    WALLETICON,
+    HERO,
+}

1d3eb6bb8cab930723f81f7544f134f688fedfba
yuslove1 <adexyuslove@gmail.com>
2025-03-23 11:44:47 -0700

    feat: refactore the entry screen

--- sheda-solution-mobile/index.js
+++ sheda-solution-mobile/index.js
@@ -1,11 +1,17 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot }  from 'expo-router';
 import "./global.css"
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context('./app');
-  return <ExpoRoot context={ctx} />;
+  return (
+    <SafeAreaProvider >
+      <SafeAreaView style={{width: deviceWidths, height: deviceHeight,}}>
+      <ExpoRoot context={ctx} />;
+      </SafeAreaView>
+    </SafeAreaProvider>
+  ) 
 }
-
 registerRootComponent(App);

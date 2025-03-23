ec27eb728ba2fd897a6c46593ced9f565954ad62
yuslove1 <adexyuslove@gmail.com>
2025-03-23 11:48:13 -0700

    feat: onboarding screen

--- sheda-solution-mobile/app/index.tsx
+++ sheda-solution-mobile/app/index.tsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Link, router } from "expo-router";
+import Button from "@/components/common/Button";
+import InterSemiBold from "@/components/Text/InterSemiBold";
+import { ImageBackground } from "react-native";
+import { HERO } from "@/constants/images-icons";
+import InterRegular from "@/components/Text/InterRegular";
 
-export default function Index() {
+export default function Onboarding() {
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      <View className="h-[60%] w-full">
+        <ImageBackground
+          resizeMode="cover"
+          source={HERO}
+          style={{ width: "100%", height: "100%" }}
+        ></ImageBackground>
+      </View>
+
+      <View className="flex-1 bg-background rounded-t-2xl lg:rounded-t-none -mt-5 lg:mt-none p-5">
+        <View className="mt-5 flex items-center">
+          <InterSemiBold className="text-xl lg:text-4xl mb-2">Welcome to advanced real estate</InterSemiBold>
+          <InterRegular className="text-secondaryText text-base lg:text-2xl">Sheda solutions makes your real</InterRegular>
+          <InterRegular className="text-secondaryText text-base lg:text-2xl">estate operations very easy and stress-free.</InterRegular>
+        </View>
+
+        <View className="mt-16">
+          <Button 
+          isFull={true}
+          className="rounded-lg"
+          onPress={() => router.push('/(auth)/login')}
+          >
+            <InterSemiBold className="text-background">Sign in</InterSemiBold>
+          </Button>
+          <Button 
+          isFull={true} 
+          color="tranparent"
+          onPress={() => router.push('/(auth)/signup')}
+          className="rounded-lg border border-borderColor">
+            <InterSemiBold className="">Create account</InterSemiBold>
+          </Button>
+        </View>
+      </View>
     </View>
   );
 }

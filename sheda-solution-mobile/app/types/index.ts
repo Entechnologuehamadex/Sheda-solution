ab16cf9c8d704959d98a2e86ecb6c06d9b40e79e
yuslove1 <adexyuslove@gmail.com>
2025-03-23 11:55:18 -0700

    types

---
+++ sheda-solution-mobile/app/types/index.ts
@@ -0,0 +1,6 @@
+import { ImageSourcePropType } from "react-native";
+export interface IconProps {
+    source: ImageSourcePropType; // Type for image source
+    size?: number; // Optional size prop
+    color?: string; // Optional color prop
+  }

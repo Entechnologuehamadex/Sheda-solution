c0128ac1ca03d61e19852203b2b1df78b01c8513
yuslove1 <adexyuslove@gmail.com>
2025-03-23 11:54:00 -0700

    feat: tabs screens layout designed

---
+++ sheda-solution-mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,95 @@
+import { Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Octicons from "@expo/vector-icons/Octicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { View } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#C1272D",
+        tabBarInactiveTintColor: "#000000",
+        tabBarStyle: {
+          backgroundColor: "#ffffff",
+          borderTopWidth: 3,
+          borderTopColor: "#e0e0e0",
+          height: 60,
+          paddingBottom: 10,
+        },
+        tabBarLabelStyle: {
+          fontFamily: "Manrope-Medium",
+          fontSize: 12,
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size, focused }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={focused ? "#C1272D" : "black"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved/index"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color, size, focused }) => (
+            <AntDesign
+              name="hearto"
+              size={24}
+              color={focused ? "#C1272D" : "black"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history/index"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Octicons
+              name="history"
+              size={24}
+              color={focused ? "#C1272D" : "black"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox/index"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons
+              name="comment-text-outline"
+              size={24}
+              color={focused ? "#C1272D" : "black"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wallet/index"
+        options={{
+          title: "Wallet",
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialCommunityIcons
+              name="wallet-outline"
+              size={24}
+              color={focused ? "#C1272D" : "black"}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}

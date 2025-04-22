b8bcae53a3d52815878f108bcc894dd4ed258123
yuslove1 <adexyuslove@gmail.com>
2025-04-22 04:29:44 -0700

    icon

--- sheda-solution-mobile/assets/icons/index.ts
+++ sheda-solution-mobile/assets/icons/index.ts
@@ -1,9 +1,23 @@
 import GoogleIcon from './social-icon/Google.svg';
 import FacebookIcon from './social-icon/facebook.svg';
 import AppleIcon from './social-icon/apple.svg';
+import ArrowBack from './chevron.left.svg'
+import Profile from './profile.svg';
+import Setting from './setting.svg';
+import purchase from './no-purchase.svg'
+import message from './message.svg'
+import deposit from './deposit.svg';
+import withdraw from './withdraw.svg'
 
 export { 
     GoogleIcon as GOOGLEICON,
     FacebookIcon as FACEBOOKICON,
     AppleIcon as APPLEICON,
+    ArrowBack as ARROWBACK,
+    Profile as PROFILE,
+    Setting as SETTING,
+    purchase as NOPURCHASE,
+    message as MESSAGE,
+    deposit as DEPOSIT,
+    withdraw as WITHDRAW,
 };

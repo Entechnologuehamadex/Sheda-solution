6381c9cef0d929e353c6a6bca9ba9ddde8d1cb95
yuslove1 <adexyuslove@gmail.com>
2025-04-22 04:34:27 -0700

    feat: no purchase yet component

---
+++ sheda-solution-mobile/components/NoPurchase.tsx
@@ -0,0 +1,40 @@
+import { View } from "react-native"
+import Icon from "./common/Icon"
+import { NOPURCHASE } from "@/assets/icons"
+import InterSemiBold from "./Text/InterSemiBold"
+import InterRegular from "./Text/InterRegular"
+import Button from "./common/Button"
+import { router } from "expo-router"
+export const NoPurchase = () => {
+    return(
+
+        <View className="flex-1 justify-center">
+          <View className="items-center">
+            <Icon
+              icon={NOPURCHASE}
+              width={30}
+              height={35}
+              className="w-[90] h-[95] justify-center items-center bg-[#D8DADC26] rounded-full"
+            />
+            <View className="my-4 items-center">
+              <InterSemiBold className="text-lg/5">
+                You have not made any purchase yet...
+              </InterSemiBold>
+              <InterRegular className="text-base/5 text-secondaryText">
+                Ongoing purchase will show here
+              </InterRegular>
+            </View>
+            <Button 
+            isFull
+            className="rounded-md"
+            onPress={() => router.push('/(tabs)/home')}
+            >
+              <InterSemiBold className="text-white text-md">
+                Back to homepage
+              </InterSemiBold>
+            </Button>
+          </View>
+        </View>
+    )
+}
+

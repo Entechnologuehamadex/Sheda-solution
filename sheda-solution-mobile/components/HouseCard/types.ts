9731509e2363adc52aed91db56567294738f2aa4
yuslove1 <adexyuslove@gmail.com>
2025-05-13 10:47:24 -0700

    types

--- sheda-solution-mobile/components/HouseCard/types.ts
+++ sheda-solution-mobile/components/HouseCard/types.ts
@@ -5,5 +5,29 @@ export interface HouseCardProps{
     location: string;
     bedrooms: number;
     bathrooms: number;
+    description: string;
+    mode: 'rent' | 'buy';
+    extras: string[];
     image?: string | {uri: string} | any;
+    seller?: {
+        name: string;
+        phone: string;
+        photo: string | {uri: string} | any;
+        isActive?: boolean;
+        rating?: number | any;
+        reviews?: REVIEW[];
+    },
+    isFavorite?: boolean;
+    isSold?: boolean;
+    isRented?: boolean;
+    isAvailable?: boolean;
+
+}
+
+export interface REVIEW{
+        id: string;
+        name: string;
+        comment: string;
+        date: string;
+        length: any;
 }

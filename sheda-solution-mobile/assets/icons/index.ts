aa8fe1dde618a67ac0c0aa786a159b69532d86e8
yuslove1 <adexyuslove@gmail.com>
2025-05-13 10:46:26 -0700

    icons export

--- sheda-solution-mobile/assets/icons/index.ts
+++ sheda-solution-mobile/assets/icons/index.ts
@@ -7,7 +7,18 @@ import Setting from './setting.svg';
 import purchase from './no-purchase.svg'
 import message from './message.svg'
 import deposit from './deposit.svg';
-import withdraw from './withdraw.svg'
+import withdraw from './withdraw.svg';
+import tick from './profile-tick.svg';
+import profileSetting from './profileSetting.svg';
+import help from './help.svg';
+import logout from './logout.svg'
+import chevronRight from './chevron-right.svg';
+import heart from './heart.svg';
+import filledHeart from './filled-heart.svg';
+import location from './location.svg';
+import bed from './bed.svg';
+import bath from './bath.svg';
+import phone from './phone.svg'
 
 export { 
     GoogleIcon as GOOGLEICON,
@@ -20,4 +31,15 @@ export {
     message as MESSAGE,
     deposit as DEPOSIT,
     withdraw as WITHDRAW,
+    tick as PROFILETICK,
+    profileSetting as PROFILESETTING,
+    help as HELP,
+    logout as LOGOUT,
+    chevronRight as CHEVRONRIGHT,
+    heart as HEART,
+    filledHeart as FILLEDHEART,
+    location as LOCATION,
+    bed as BED,
+    bath as BATH,
+    phone as PHONE,
 };

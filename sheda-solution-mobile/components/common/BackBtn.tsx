36a8c3a3c688d671e8763d953a5a968b7b6616fc
yuslove1 <adexyuslove@gmail.com>
2025-05-13 10:44:56 -0700

    feat: back button component

--- sheda-solution-mobile/components/common/BackBtn.tsx
+++ sheda-solution-mobile/components/common/BackBtn.tsx
@@ -3,12 +3,16 @@ import { ARROWBACK } from "@/assets/icons";
 import { useRouter } from "expo-router";
 import Icon from "./Icon";
 
-const BackBtn = () => {
+interface BackBtnProps{
+    className?: string;
+}
+
+const BackBtn = ({className}: BackBtnProps) => {
      
     const router = useRouter()
 
     return(
-        <TouchableOpacity onPress={() => router.back()} className="border w-12 h-12 items-center justify-center border-borderColor rounded-xl">
+        <TouchableOpacity onPress={() => router.back()} className={`border w-12 h-12 items-center justify-center border-borderColor rounded-xl ${className}`}>
          <View pointerEvents="none">
           <Icon
           icon={ARROWBACK}

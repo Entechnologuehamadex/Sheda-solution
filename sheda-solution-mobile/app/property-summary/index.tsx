9cfbbd35d75888cfbf84a2c287446b292d8ea5f1
yuslove1 <adexyuslove@gmail.com>
2025-05-15 19:48:45 -0700

    feat: added property summary screen

---
+++ sheda-solution-mobile/app/property-summary/index.tsx
@@ -0,0 +1,143 @@
+import BackBtn from "@/components/common/BackBtn";
+import InterSemiBold from "@/components/Text/InterSemiBold";
+import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import getDetails from "../details/getDetails";
+import Icon from "@/components/common/Icon";
+import { LOCATION } from "@/assets/icons";
+import InterRegular from "@/components/Text/InterRegular";
+import InterMedium from "@/components/Text/InterMedium";
+import InterExtraLight from "@/components/Text/InterExtraLight";
+import totalPaymemt from "@/utilities/totalPayment";
+import Button from "@/components/common/Button";
+
+const PropertySummary = () => {
+  // Safely extract id from search params
+  const { id } = useLocalSearchParams();
+  const propertyId = id; // Handle string | string[]
+  let property = getDetails(propertyId as string); //grab the property from the backend
+
+  const totalPay = totalPaymemt(property?.price, property?.damages);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView
+        className="container flex-1 mx-auto max-w-2xl p-5"
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+        showsVerticalScrollIndicator={false}
+        // style={{ paddingVertical: 20 }}
+      >
+        {/* header */}
+        <View className="flex-row gap-4 items-center">
+          <BackBtn />
+          <InterSemiBold className="text-base/5">
+            Property Summary
+          </InterSemiBold>
+        </View>
+
+        <View className="mt-7 border-b border-borderColor pb-4">
+          <View className="h-[160] w-full rounded-lg overflow-hidden">
+            <Image
+              source={property?.image}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </View>
+
+          <View className="my-3">
+            <InterMedium className="text-lg/5 mt-4">
+              {property?.type}
+            </InterMedium>
+            <View className="flex-row items-center gap-1 mt-1">
+              <Icon icon={LOCATION} width={12} height={16} />
+              <InterRegular className="text-sm/5 text-secondaryText mt-1">
+                {property?.location}
+              </InterRegular>
+            </View>
+          </View>
+
+          <View>
+            <InterRegular className="text-sm/5 ">Description</InterRegular>
+            <InterRegular className="text-sm/5 text-secondaryText mt-1">
+              {property?.description}
+            </InterRegular>
+          </View>
+
+          <View className="mt-3">
+            <InterRegular className="text-sm/5 ">Listing duration</InterRegular>
+            <InterRegular className="text-sm/5 text-secondaryText mt-1">
+              duration placeholder
+            </InterRegular>
+          </View>
+
+          {property?.mode === "rent" && (
+            <View>
+              <View className="flex-row justify-between mt-3">
+                <InterRegular className="text-xs/[150%]">Rent/yr</InterRegular>
+                <InterExtraLight className="text-xs/[150%]">
+                  {property.price}
+                </InterExtraLight>
+              </View>
+              <View className="flex-row justify-between mt-3">
+                <InterRegular className="text-xs/[150%]">Damages</InterRegular>
+                <InterExtraLight className="text-xs/[150%]">
+                  {property.damages}
+                </InterExtraLight>
+              </View>
+            </View>
+          )}
+        </View>
+
+        <View className="flex-row justify-between mt-3">
+          <InterRegular>Total</InterRegular>
+          <View className="">
+            <InterSemiBold className="text-right">N{totalPay}</InterSemiBold>
+            {/* <InterRegular className="text-xs/[150%] text-secondaryText">
+              Available: Act bal placeholder
+            </InterRegular> */}
+          </View>
+        </View>
+
+        <View className="border-t border-dotted mt-8">
+          <View className="flex-row justify-between mt-10">
+            <InterMedium className="text-xs/[150%]">Payment method</InterMedium>
+            <InterRegular className="text-xs/[150%]">
+              Selected method
+            </InterRegular>
+          </View>
+          <View className="flex-row justify-between my-3">
+            <InterMedium className="text-xs/[150%]">Charge</InterMedium>
+            <InterSemiBold className="text-xs/[150%]">
+              Amount in selected currency
+            </InterSemiBold>
+          </View>
+          <View className="flex-row justify-between">
+            <InterMedium className="text-xs/[150%]">Rate</InterMedium>
+            <InterRegular className="text-xs/[150%]">
+              Per/Selected rate
+            </InterRegular>
+          </View>
+        </View>
+      </ScrollView>
+
+      <View className="absolute bottom-0 left-0 right-0 mx-auto max-w-2xl py-5 px-5">
+        <Button
+          onPress={() =>
+            router.push({
+              pathname: "/payment-pin",
+              params: { id: propertyId, amount: totalPay },
+            })
+          }
+          className="rounded-lg"
+        >
+          <InterRegular className="text-base/5 text-white">
+            Confirm payment
+          </InterRegular>
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default PropertySummary;
--- sheda-solution-mobile/app/select-currency/index.tsx
+++ sheda-solution-mobile/app/select-currency/index.tsx
@@ -4,8 +4,13 @@ import InterSemiBold from "@/components/Text/InterSemiBold";
 import InterRegular from "@/components/Text/InterRegular";
 import CurrencyList from "./CurrencyList";
 import Button from "@/components/common/Button";
+import InterMedium from "@/components/Text/InterMedium";
+import { useLocalSearchParams, router } from "expo-router";
 
 const SelectCurrency = () => {
+  const { id } = useLocalSearchParams();
+  const propertyId = id; // Handle string | string[]
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView
@@ -22,19 +27,29 @@ const SelectCurrency = () => {
           <InterSemiBold className="text-base/5">Select currency</InterSemiBold>
         </View>
 
-        <View className="px-5 py-3 mt-5">
-            <InterRegular className="text-sm">Available assets</InterRegular>
-            <InterRegular className="text-xs/[150%] text-secondaryText">The cost equivalent will be charged from your available balance</InterRegular>
+        <View className="px-5 py-3 mt-5 mb-2">
+          <InterRegular className="text-sm">Available assets</InterRegular>
+          <InterRegular className="text-xs/[150%] text-secondaryText">
+            The cost equivalent will be charged from your available balance
+          </InterRegular>
         </View>
 
         {/* dummy currency list */}
         <CurrencyList />
       </ScrollView>
-      <View className="absolute bottom-0 left-0 right-0 mx-auto max-w-2xl py-5">
-            <Button className="rounded-lg">
-                <InterRegular className="text-white">Proceed</InterRegular>
-            </Button>
-        </View>
+      <View className="absolute bottom-0 left-0 right-0 mx-auto max-w-2xl p-5">
+        <Button
+          onPress={() =>
+            router.push({
+              pathname: '/property-summary',
+              params: { id: propertyId },
+            })
+          }
+          className="rounded-lg"
+        >
+          <InterMedium className="text-white">Proceed</InterMedium>
+        </Button>
+      </View>
     </SafeAreaView>
   );
 };

c245ffc839893c88e0edc8d48d4ffd4a10a9459d
yuslove1 <adexyuslove@gmail.com>
2025-05-28 08:16:44 -0700

    assets

--- sheda-solution-mobile/assets/icons/index.ts
+++ sheda-solution-mobile/assets/icons/index.ts
@@ -4,6 +4,7 @@ import AppleIcon from './social-icon/apple.svg';
 import ArrowBack from './chevron.left.svg'
 import Profile from './profile.svg';
 import Setting from './setting.svg';
+import Search from './search.svg';
 import purchase from './no-purchase.svg'
 import message from './message.svg'
 import deposit from './deposit.svg';
@@ -20,6 +21,20 @@ import bed from './bed.svg';
 import bath from './bath.svg';
 import phone from './phone.svg'
 import successful from './successful.svg';
+import toggler from './toggle.svg';
+import mediaPlus from './media-plus.svg';
+import audioRecord from './audio-record.svg'; 
+import bin from './bin.svg';
+import list from './list.svg';
+import flag from './flag.svg';
+import blackBin from './black-bin.svg';
+import lock from './lock.svg';
+import notification from './notication.svg';
+import call from './call.svg';
+import inbox from './inbox.svg'
+import pencil from './pencil.svg';
+import eye from './eye.svg';
+import eyeSlash from './eye-slash.svg';
 
 export { 
     GoogleIcon as GOOGLEICON,
@@ -28,6 +43,7 @@ export {
     ArrowBack as ARROWBACK,
     Profile as PROFILE,
     Setting as SETTING,
+    Search as SEARCH,
     purchase as NOPURCHASE,
     message as MESSAGE,
     deposit as DEPOSIT,
@@ -43,5 +59,19 @@ export {
     bed as BED,
     bath as BATH,
     phone as PHONE,
-    successful as SUCCESSFUL
+    successful as SUCCESSFUL,
+    toggler as TOGGLER,
+    mediaPlus as MEDIAPLUS,
+    audioRecord as AUDIORECORD,
+    bin as BIN,
+    list as LIST,
+    flag as FLAG,
+    blackBin as BLACKBIN,
+    lock as LOCK,
+    notification as NOTIFICATION,
+    inbox as INBOX,
+    call as CALL,
+    pencil as PENCIL,
+    eye as EYE,
+    eyeSlash as EYESLASH
 };

dd4db6da4fffd2220c97f878ed889df6fdd584e6
yuslove1 <adexyuslove@gmail.com>
2025-05-28 08:23:39 -0700

    types

--- sheda-solution-mobile/components/input/textInput/types.ts
+++ sheda-solution-mobile/components/input/textInput/types.ts
@@ -3,5 +3,6 @@ import { TextInputProps } from "react-native";
 export interface StyledTextInputProps extends TextInputProps {
     placeholder?: string;
     isPassword?: boolean;
+    label?: string;
     onPress?: () => void;
   }
--- sheda-solution-mobile/types/index.ts
+++ sheda-solution-mobile/types/index.ts
@@ -4,3 +4,54 @@ export interface IconProps {
     size?: number; // Optional size prop
     color?: string; // Optional color prop
   }
+
+
+  export interface Seller {
+    name: string;
+    phone: string;
+    photo: any; // Image source (e.g., require('../path'))
+    rating: number;
+    isActive: boolean;
+    reviews: {
+      id: string;
+      name: string;
+      comment: string;
+      date: string;
+    }[];
+  }
+  
+  export interface HouseProps {
+    id: string;
+    price: string;
+    type: string;
+    location: string;
+    bedrooms: number;
+    bathrooms: number;
+    mode?: "buy" | "rent"; 
+    description?: string;
+    extras?: string[];
+    image: any; // Image source
+    seller?: Seller;
+    isFavorite?: boolean;
+    damages?: string; // Added for Active
+  }
+  
+  export interface CancelledHistory {
+    id: string;
+    image: any;
+    type: string;
+    location: string;
+    price: string;
+    bedrooms: number;
+    bathrooms: number;
+    reason: string;
+    cancelledDate: string;
+    mode?: "buy" | "rent";
+  }
+  
+  export interface HistoryData {
+    Ongoing: HouseProps[];
+    Active: HouseProps[];
+    Cancelled: CancelledHistory[];
+    Expired: any[];
+  }
